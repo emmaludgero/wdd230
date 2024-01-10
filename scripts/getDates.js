aa5695c76899c15161de0b055c25fbcf04e64cbe
emmaludgero <emma.ludgero@gmail.com>
2024-01-09 23:51:14 -0300

    W01: Your Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,14 @@
+// Variables
+
+let oLastModif = new Date(document.lastModified);
+let actualYear = new Date().toLocaleDateString("en-US", { year: "numeric" })
+
+console.log();
+
+// Modifing HTML
+
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = oLastModif.toLocaleDateString("en-US", { day: "numeric", month: "numeric", year: "numeric", hour: "2-digit", minute: "2-digit" })
+
+const year = document.getElementById("year");
+year.textContent = actualYear;
