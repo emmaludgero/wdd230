e79f2d86c83e08ab70d40e25faf3b45d32fc68c7
emmaludgero <emma.ludgero@gmail.com>
2024-01-19 19:24:34 -0300

    init chamber

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,17 @@
+// Variables
+
+let oLastModif = new Date(document.lastModified);
+let actualYear = new Date().toLocaleDateString("en-US", { year: "numeric" })
+let flag = '\u{1F1E7}\u{1F1F7}';
+
+console.log(flag);
+
+// Modifing HTML
+
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = oLastModif.toLocaleDateString("en-US", { day: "numeric", month: "numeric", year: "numeric", hour: "2-digit", minute: "2-digit" })
+
+const year = document.getElementById("year");
+year.textContent = actualYear;
+
+document.getElementById("flag").textContent = flag;
