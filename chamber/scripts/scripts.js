174c2558b372cc9c89e6468cc179cd13496a46d2
emmaludgero <emma.ludgero@gmail.com>
2024-01-21 01:11:13 -0300

    Chamber Home Page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -11,7 +11,41 @@ console.log(flag);
 const lastModified = document.getElementById('lastModified');
 lastModified.textContent = oLastModif.toLocaleDateString("en-US", { day: "numeric", month: "numeric", year: "numeric", hour: "2-digit", minute: "2-digit" })
 
-const year = document.getElementById("year");
-year.textContent = actualYear;
-
 document.getElementById("flag").textContent = flag;
+
+// Evento para copiar o endereço de email ao clicar nele. 
+
+document.getElementById('emailLink').addEventListener('click', function () {
+    // Obtém o endereço de e-mail do atributo href
+    var email = this.getAttribute('href').replace('mailto:', '');
+    // Cria um elemento de texto temporário
+    var tempElement = document.createElement('textarea');
+    // Define o valor do elemento de texto como o endereço de e-mail
+    tempElement.value = email;
+    // Adiciona o elemento de texto ao corpo do documento
+    document.body.appendChild(tempElement);
+    // Seleciona e copia o conteúdo do elemento de texto
+    tempElement.select();
+    document.execCommand('copy');
+    // Remove o elemento de texto temporário
+    document.body.removeChild(tempElement);
+    //**  Alerta o usuário que o endereço de e-mail foi copiado
+    Swal.fire({
+        text: 'Email copied',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 1500   // Aqui você pode definir o ícone desejado
+    });
+    // Impede o comportamento padrão do link
+    event.preventDefault();
+});
+
+// Hamburguer menu
+
+const nav = document.querySelector('.navigation');
+const menuButton = document.querySelector('#menu');
+
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    menuButton.classList.toggle('show');
+})
