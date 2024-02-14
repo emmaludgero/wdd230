bf5c1a9f32b1d65147ab6b99f157d5886410e9e1
emmaludgero <emma.ludgero@gmail.com>
2024-02-14 17:32:38 -0300

    ajustes w03

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -54,3 +54,11 @@ modeButton.addEventListener("click", () => {
         });
     }
 });
+
+// Number of Visits Code
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+visitsDisplay.textContent = numVisits;
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
