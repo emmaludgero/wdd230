e5c3c37c48c4f61902394a321b009f9dab5edeaf
emmaludgero <emma.ludgero@gmail.com>
2024-02-17 17:49:24 -0300

    Push Form

---
+++ scripts/form.js
@@ -0,0 +1,34 @@
+// Rating
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+// Matching passwords
+
+const pss = document.querySelector("#pss");
+const pss2 = document.querySelector("#pss2");
+const message = document.querySelector("#formmessage");
+
+pss2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    if (pss.value !== pss2.value) {
+        message.textContent = "❗Key Phrases DO NOT MATCH!";
+        message.style.visibility = "show";
+        pss2.style.backgroundColor = "#fff0f3";
+        pss2.value = "";
+        pss2.focus();
+    } else {
+        message.style.display = "none";
+        pss2.style.backgroundColor = "#fff";
+        pss2.style.color = "#000";
+    }
+}
