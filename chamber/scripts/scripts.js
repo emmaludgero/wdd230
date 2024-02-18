41c6df709ee0168294937e894c953072348385c2
emmaludgero <emma.ludgero@gmail.com>
2024-02-17 23:57:39 -0300

    Join page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -3,6 +3,7 @@
 let oLastModif = new Date(document.lastModified);
 let actualYear = new Date().toLocaleDateString("en-US", { year: "numeric" })
 let flag = '\u{1F1E7}\u{1F1F7}';
+let currentDateTime = new Date().toLocaleString();
 
 console.log(flag);
 
@@ -15,7 +16,7 @@ document.getElementById("flag").textContent = flag;
 
 // Evento para copiar o endereço de email ao clicar nele. 
 
-document.getElementById('emailLink').addEventListener('click', function () {
+document.getElementById('emailLink').addEventListener('click', function (event) {
     // Obtém o endereço de e-mail do atributo href
     var email = this.getAttribute('href').replace('mailto:', '');
     // Cria um elemento de texto temporário
@@ -49,3 +50,19 @@ menuButton.addEventListener('click', () => {
     nav.classList.toggle('show');
     menuButton.classList.toggle('show');
 })
+
+// Timestamp
+
+document.getElementById('timestamp').value = currentDateTime;
+
+// Validates pattern
+
+window.onload = function () {
+    document.getElementById('meuFormulario').addEventListener('submit', function (event) {
+        var positionInput = document.getElementById('position');
+        if (!positionInput.checkValidity()) {
+            alert('Please fill in the field with at least seven alphabetic characters, spaces, or hyphens.');
+            event.preventDefault(); // Impede o envio do formulário se a validação falhar
+        }
+    });
+};
