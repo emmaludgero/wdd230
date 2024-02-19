918eb2d1dcff57a5e07f7b928bbd9098e913d373
emmaludgero <emma.ludgero@gmail.com>
2024-02-19 18:15:21 -0300

    Regex Working!

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,68 +1,104 @@
-// Variables
+document.addEventListener('DOMContentLoaded', function () {
 
-let oLastModif = new Date(document.lastModified);
-let actualYear = new Date().toLocaleDateString("en-US", { year: "numeric" })
-let flag = '\u{1F1E7}\u{1F1F7}';
-let currentDateTime = new Date().toLocaleString();
+    // Variables
 
-console.log(flag);
+    let oLastModif = new Date(document.lastModified);
+    let actualYear = new Date().toLocaleDateString("en-US", { year: "numeric" })
+    let flag = '\u{1F1E7}\u{1F1F7}';
+    let currentDateTime = new Date().toLocaleString();
 
-// Modifing HTML
+    console.log(currentDateTime);
 
-const lastModified = document.getElementById('lastModified');
-lastModified.textContent = oLastModif.toLocaleDateString("en-US", { day: "numeric", month: "numeric", year: "numeric", hour: "2-digit", minute: "2-digit" })
+    // Modifing HTML
 
-document.getElementById("flag").textContent = flag;
+    const lastModified = document.getElementById('lastModified');
+    lastModified.textContent = oLastModif.toLocaleDateString("en-US", { day: "numeric", month: "numeric", year: "numeric", hour: "2-digit", minute: "2-digit" })
 
-// Evento para copiar o endereço de email ao clicar nele. 
+    document.getElementById("flag").textContent = flag;
 
-document.getElementById('emailLink').addEventListener('click', function (event) {
-    // Obtém o endereço de e-mail do atributo href
-    var email = this.getAttribute('href').replace('mailto:', '');
-    // Cria um elemento de texto temporário
-    var tempElement = document.createElement('textarea');
-    // Define o valor do elemento de texto como o endereço de e-mail
-    tempElement.value = email;
-    // Adiciona o elemento de texto ao corpo do documento
-    document.body.appendChild(tempElement);
-    // Seleciona e copia o conteúdo do elemento de texto
-    tempElement.select();
-    document.execCommand('copy');
-    // Remove o elemento de texto temporário
-    document.body.removeChild(tempElement);
-    //**  Alerta o usuário que o endereço de e-mail foi copiado
-    Swal.fire({
-        text: 'Email copied',
-        icon: 'success',
-        showConfirmButton: false,
-        timer: 1500   // Aqui você pode definir o ícone desejado
+    // Evento para copiar o endereço de email ao clicar nele. 
+
+    document.getElementById('emailLink').addEventListener('click', function (event) {
+        // Obtém o endereço de e-mail do atributo href
+        var email = this.getAttribute('href').replace('mailto:', '');
+        // Cria um elemento de texto temporário
+        var tempElement = document.createElement('textarea');
+        // Define o valor do elemento de texto como o endereço de e-mail
+        tempElement.value = email;
+        // Adiciona o elemento de texto ao corpo do documento
+        document.body.appendChild(tempElement);
+        // Seleciona e copia o conteúdo do elemento de texto
+        tempElement.select();
+        document.execCommand('copy');
+        // Remove o elemento de texto temporário
+        document.body.removeChild(tempElement);
+        //**  Alerta o usuário que o endereço de e-mail foi copiado
+        Swal.fire({
+            text: 'Email copied',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 1500   // Aqui você pode definir o ícone desejado
+        });
+        // Impede o comportamento padrão do link
+        event.preventDefault();
     });
-    // Impede o comportamento padrão do link
-    event.preventDefault();
-});
+    // Hamburguer menu
 
-// Hamburguer menu
+    const nav = document.querySelector('.navigation');
+    const menuButton = document.querySelector('#menu');
 
-const nav = document.querySelector('.navigation');
-const menuButton = document.querySelector('#menu');
+    menuButton.addEventListener('click', () => {
+        nav.classList.toggle('show');
+        menuButton.classList.toggle('show');
+    })
 
-menuButton.addEventListener('click', () => {
-    nav.classList.toggle('show');
-    menuButton.classList.toggle('show');
-})
+    // Timestamp
 
-// Timestamp
+    document.addEventListener('DOMContentLoaded', function () {
 
-document.getElementById('timestamp').value = currentDateTime;
+        document.getElementById('timestamp').value = currentDateTime;
+    });
 
-// Validates pattern
+    // Validates pattern
 
-window.onload = function () {
-    document.getElementById('meuFormulario').addEventListener('submit', function (event) {
-        var positionInput = document.getElementById('position');
-        if (!positionInput.checkValidity()) {
-            alert('Please fill in the field with at least seven alphabetic characters, spaces, or hyphens.');
-            event.preventDefault(); // Impede o envio do formulário se a validação falhar
+    window.onload = function () {
+        document.getElementById('forms').addEventListener('submit', function (event) {
+            var positionInput = document.getElementById('position').value;
+            var regex = /^[a-zA-Z -]{7,}$/;
+            if (!regex.test(positionInput)) {
+                alert('Please fill in the field with at least seven alphabetic characters, spaces, or hyphens.');
+                event.preventDefault(); // Impede o envio do formulário se a validação falhar
+            }
+        });
+    };
+
+    // Last Visit
+
+    const msPerDay = 86400000;
+    const thisVisit = new Date();
+
+    //display elements
+    const firstVisitMsg = "Welcome! Let us know if you have any questions";
+    const soonVisitMsg = "Back so soon! Awesome!";
+
+    //get item date of last visit to local storage
+    let visitDate = localStorage.getItem("visitDate-ls");
+
+    let daysSinceVisit = visitDate ? (thisVisit - new Date(visitDate)) / msPerDay : 0;
+
+    if (daysSinceVisit !== 0) {
+
+        if (daysSinceVisit > 0 && daysSinceVisit < 1) {
+            document.querySelector("#visit-msg").textContent = soonVisitMsg;
+        } else if (daysSinceVisit > 1) {
+            document.querySelector("#visit-msg").textContent = `You last visited ${Math.floor(daysSinceVisit)} days ago`;
+
         }
-    });
-};
+    } else {
+        document.querySelector("#visit-msg").textContent = firstVisitMsg;
+    }
+
+    //set item date of current visit to local storage
+    localStorage.setItem("visitDate-ls", thisVisit);
+
+});
