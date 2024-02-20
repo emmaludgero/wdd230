01f8bf072e81626058a12e9a5d70f32d601c3c1e
emmaludgero <emma.ludgero@gmail.com>
2024-02-19 23:57:50 -0300

    json

--- scripts/links.js
+++ scripts/links.js
@@ -1,27 +1,45 @@
 const baseURL = "https://emmaludgero.github.io/wdd230/";
-const linksURL = "https://emmaludgero.github.io/wdd230/data/links.json";
+const linksURL = "https://github.com/emmaludgero/wdd230/blob/main/data/liks.json";
 
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log("Links: ", data);
-    document.getElementById('links').innerHTML = displayLinks(data);
+async function getLinks(url) {
+    console.log("A");
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log("Response: ", JSON.stringify(response));
+        displayLinks(data.weeks);
+    } catch (error) {
+        console.log('Error fetching data:', error);
+    }
 }
 
-getLinks();
 
-function displayLinks(data) {
-    let html = '<ul>';
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
 
-    data.weeks.forEach(week => {
-        html += `<li><strong>${week.week}:</strong> `;
-        week.links.forEach(link => {
-            html += `<a href="${link.url}">${link.title}</a> | `;
-        });
-        // Remove o último " | " adicionado
-        html = html.slice(0, -2);
-        html += '</li>';
+        let links = document.querySelector("#links");
+
+        let weekNumbers = document.createElement("li");
+
+        weekNumbers.textContent = `${week.week}:  `;
+        links.appendChild(weekNumbers);
+
+        week.links.forEach((link) => {
+
+            let task = document.createElement("a");
+
+            task.textContent = link.title;
+            task.setAttribute("href", link.url)
+
+            let divider = document.createElement("span");
+            divider.textContent = " | ";
+
+
+            weekNumbers.appendChild(task);
+            weekNumbers.appendChild(divider);
+
+        })
     });
-
-    html += '</ul>';
 }
+
+getLinks(linksURL);
